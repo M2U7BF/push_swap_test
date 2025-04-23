93ee79b12155e208ed5f98c38f28c6f901f4fb6b
M2U7BF <yhvh4321@gmail.com>
2025-04-23 09:58:05 +0900

    update

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+make re
+
+ARG=(`ruby -e 'print (1..100).to_a.shuffle * " "'`); echo $ARG; ./push_swap $ARG | tee >(cat) | ./checker $ARG
+# ruby -e 'N = 100; (1..N).to_a.permutation.each{ |arr| puts "[#{arr * ", "}]"; `./push_swap #{arr * " "} > res`; puts `./checker #{arr * " "}; wc -l res`  }'
+# ARG=(`ruby -e 'print (1..100).to_a.shuffle * " "'`); ./push_swap $ARG | tee >(wc -l) | ./checker $ARG
