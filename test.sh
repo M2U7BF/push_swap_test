4b1dc5c147ea2c021e73a887d5a8dee2e6b659ff
M2U7BF <yhvh4321@gmail.com>
2025-05-08 06:50:49 +0900

    update

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 make re
 
+test -f checker || wget -O checker https://cdn.intra.42.fr/document/document/33389/checker_linux
+
 ARG=(`ruby -e 'print (1..100).to_a.shuffle * " "'`); echo "${ARG[@]}"; ./push_swap "${ARG[@]}"
 # ARG=(`ruby -e 'print (1..100).to_a.shuffle * " "'`); echo $ARG; ./push_swap $ARG | tee >(cat) | ./checker $ARG
 # ruby -e 'N = 100; (1..N).to_a.permutation.each{ |arr| puts "[#{arr * ", "}]"; `./push_swap #{arr * " "} > res`; puts `./checker #{arr * " "}; wc -l res`  }'
